8914d95988469b486c0d27a703d1457db0f8cbb9
rolzar <otakurolando@gmail.com>
2022-08-08 14:44:01 -0700

    Add initial code for one game played

--- javascript.js
+++ javascript.js
@@ -2,11 +2,21 @@ function getComputerChoice(){
     let choice = ["Rock", "Paper","Scissor"];
     let random =  Math.floor(Math.random()*choice.length);
     return choice[random];
-
 }
 
 function playerSelection(){
     let choice = prompt("Choose: Rock, Paper, Scissor");
-    return choice
+    let fixChoice = choice[0].toUpperCase() + choice.slice(1)
+    return fixChoice
 }
 
+function playRound(selection,choice){
+    if(selection == "Rock" && choice == "Scissor" || selection == "Scissor" && choice == "Paper" || selection == "Paper" && choice == "Rock" ){
+        return console.log("You win! " + selection +  " beats " + choice + "." )
+    } else if(choice == "Rock" && selection == "Scissor" || choice == "Scissor" && selection == "Paper" || choice == "Paper" && selection == "Rock") {
+        return console.log("You loose! " + choice +  " beats " + selection + "." )   
+    } else {
+        return console.log("Its a tie")
+    }
+    
+}
