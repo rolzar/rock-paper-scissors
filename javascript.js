4b0506dca8ee3dc561cb43391fb269de26ca5ab8
rolzar <otakurolando@gmail.com>
2022-08-11 17:30:29 -0700

    Add simple gui

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
-let score;
+let score = [0,0];
+let update = ' ' + score[0] +  '-' + score[1];
 
 function getComputerChoice(){
     let choice = ["Rock", "Paper","Scissor"];
@@ -6,45 +7,74 @@ function getComputerChoice(){
     return choice[random];
 }
 
-function playerSelection(){
-    let choice = prompt("Choose: Rock, Paper, Scissor");
-    let fixChoice = choice[0].toUpperCase() + choice.slice(1)
-    return fixChoice
-}
-
 function playRound(selection,choice){
-    let result;
+    let result ='';
+    const div = document.querySelector(".results");
     if(selection == "Rock" && choice == "Scissor" || selection == "Scissor" && choice == "Paper" || selection == "Paper" && choice == "Rock" ){
-        result = "You win! " + selection +  " beats " + choice + ".";
         score[0] = score[0] + 1;
-        return result
+        update = ' ' + score[0] +  '-' + score[1];
+        result = "You win! " + selection +  " beats " + choice + "." + update;       
+        div.textContent = result;
+        return 
     } else if(choice == "Rock" && selection == "Scissor" || choice == "Scissor" && selection == "Paper" || choice == "Paper" && selection == "Rock") {
-        result = "You loose! " + choice +  " beats " + selection + ".";
         score[1] = score[1] + 1;
-        return result  
+        update = ' ' + score[0] +  '-' + score[1];
+        result = "You loose! " + choice +  " beats " + selection + "." + update;
+        div.textContent = result;
+        return  
     } else {
-        result = "Its a tie";
-        score[2] = score[2] + 1
-        return result
+        update = ' ' + score[0] +  '-' + score[1];
+        result = "Its a tie" + update;
+        div.textContent = result;
+        return 
     }
     
 }
 
-function game(){
-    let final;
-    score = [0,0,0];
-    for(let i = 0; i < 5; i++){
-        console.log(playRound(playerSelection(),getComputerChoice()));
-    }
-    if(score[0]>score[1]){
-        final = "You Won! " + score[0] + "-" + score[1] 
-        return final
-    } else if(score[0]<score[1]){
-        final = "You Lost! " + score[0] + "-" + score[1] 
-        return final
-    } else {
-        final = "It's a Tie! " + score[0] + "-" + score[1] 
-        return final
-    }
+
+function winner(random){
+    const div = document.querySelector(".results");
+    playRound(random,getComputerChoice());
     
+    if(score[0] == 5){
+        div.textContent = "You won the GAME!" + update;
+        score = [0,0]
+        
+    } else if(score[1] == 5){
+        div.textContent = "You lost the GAME!" + update;
+        score = [0,0] 
+    } 
+
 }
+
+function playerSelection(e){
+    const choice = e.target.className;
+    if(!choice) return;
+    console.log(choice);
+    winner(choice);
+
+}
+
+
+window.addEventListener('click', playerSelection);
+
+
+
+
+
+
+
+
+
+//window.addEventListener('click', function(e){
+    //const choice = e.target.className;
+    //if(!choice) return;
+    //console.log(choice);
+
+//})
+
+
+
+
+//    const div = document.querySelector('.results')
+//console.log(div);
